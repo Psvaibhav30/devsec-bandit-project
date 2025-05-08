4b55551b447c8ae889878f79ef8f1abef6098482
psvaibhav <psvaibhav30@gmail.com>
2025-05-08 07:18:59 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello from DevSec!"
+
+@app.route('/echo', methods=['POST'])
+def echo():
+    data = request.form.get('data')  # Potential security issue: unsanitized input
+    return f"Received: {data}"
+
+if __name__ == '__main__':
+    app.run(debug=True)
